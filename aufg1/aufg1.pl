0e77d60d20d6aa6b0d3b43664601bed9c57f215d
FBeck <FBeck>
2016-03-31 10:22:22 +0200

    fix stuff :

--- aufg1/aufg1.pl
+++ aufg1/aufg1.pl
@@ -1,14 +1,15 @@
 :- consult('stammbaum').
 
-father(X,Y) :- male(X), parent(X,Y).
-mother(X,Y) :- female(X), parent(X,Y).
+father(X,Y) :- parent(X,Y), male(X).
+mother(X,Y) :- parent(X,Y), female(X).
 child(X,Y) :- parent(Y,X).
 grandparent(X,Y) :- parent(Z,Y), parent(X,Z).
 grandfather(X,Y) :- grandparent(X,Y), male(X).
 grandmother(X,Y) :- grandparent(X,Y), female(X).
-fullsibling(X,Y) :- father(Z,X), father(Z,Y), mother(A,X), mother(A,Y).
-halfsibling(X,Y) :- mother(A,X), mother(A,Y); father(B,X), father(B,Y).
-halfsister(X,Y) :- female(X), halfsibling(X,Y).
-halfbrother(X,Y) :- male(X), halfsibling(X,Y).
-cousin(X,Y) :- parent(A,X), parent(B,Y), fullsibling(A,B).
-nephew(X,Y) :- child(X,A), fullsibling(Y,A).
+fullsibling(X,Y) :- father(Z,X), father(Z,Y), mother(A,X), mother(A,Y), X \= Y.
+halfsibling(X,Y) :- mother(A,X), mother(A,Y) , \+ (father(B,X), father(B,Y)), X \= Y.
+halfsibling(X,Y) :- father(B,X), father(B,Y), \+ (mother(A,X), mother(A,Y)), X \= Y.
+halfsister(X,Y) :- halfsibling(X,Y), female(X).
+halfbrother(X,Y) :- halfsibling(X,Y), male(X).
+cousin(X,Y) :- parent(A,X), fullsibling(A,B), parent(B,Y).
+nephew(X,Y) :- fullsibling(Y,A), child(X,A).
