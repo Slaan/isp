0e5d1c884ed5a3c0516ad11d2f251b02abe35761
FBeck <FBeck>
2016-04-14 12:38:14 +0200

    blub

--- aufg2/dan/ask.pl
+++ aufg2/dan/ask.pl
@@ -8,6 +8,9 @@ ask((OldGoals,Goal),[Variable],[X|[Y|[Z|R]]]) :- lex(_,X,na,_,_,_), ask(OldGoals
 ask((OldGoals,Goal),[Variable|[OldVariable|RestVariable]],[X|[Y|[Z|R]]]) :- lex(_,X,bz,_,_,_), ask(OldGoals,[OldVariable|RestVariable],[Y|[Z|R]]), Goal =.. [Y,Variable,OldVariable].
 ask(Goals,Variable,[X|R]) :- ask(Goals,Variable,R),!.
 
-xxx(F,W,[LL|L],BB) :- s([XX|X],F,[]),flatten([XX|X],XF), ask(W,[LL|L],XF), call(W), s([LL|X],BB,[]).
+writer([]) :- write('!').
+writer([H|T]) :- write(' '), write(H), writer(T).
 
-main(Goal) :- read_sentence(QuestionW),reverse(QuestionW,[M|QuestionR]), reverse(QuestionR,Question),  s([Sem|SemRest],Question,[]), flatten([Sem|SemRest],SemFlattened), ask(Goal,[Varbiale|Rest],SemFlattened), call(Goal), s([Variable|SemRest],Answer,[]).
+answering(F,W,[LL|L],BB) :- s([XX|X],F,[]),flatten([XX|X],XF), ask(W,[LL|L],XF), call(W), s([LL|X],BB,[]).
+
+main() :- read_sentence(QuestionW),reverse(QuestionW,[M|QuestionR]), reverse(QuestionR,Question), answering(Question,_,_,C), writer(C),!.
