f059c645eccc8daae002a28db49d754d19ef65e7
FBeck <FBeck>
2016-04-14 10:42:40 +0200

    gut

--- aufg2/fabi/ask.pl
+++ aufg2/fabi/ask.pl
@@ -1,7 +1,7 @@
-ask((Goal),[],[Y|[Z|[]]]) :- lex(_,X,na,_), Goal =.. [Y,X,Z].
+ask((Goal),[],[X|[Y|[Z|[]]]]) :- lex(_,X,na,_), Goal =.. [Y,X,Z].
 ask((Goal),[Variable],[Y|[Z|[]]]) :- lex(_,X,bz,_), Goal =.. [Y,Variable,Z].
 ask((OldGoals,Goal),[Variable],[X|[Y|[Z|R]]]) :- lex(_,X,na,_), ask(OldGoals,[Variable],[Y|[Z|R]]), Goal =.. [Y,X,Variable],!.
 ask((OldGoals,Goal),[Variable|[OldVariable|RestVariable]],[X|[Y|[Z|R]]]) :- lex(_,X,bz,_), ask(OldGoals,[OldVariable|RestVariable],[Y|[Z|R]]), Goal =.. [Y,Variable,OldVariable].
-ask(Goals,Variable,[X|[Y|[Z|R]]]) :- ask(Goals,Variable,[Y|[Z|R]]),!.
+ask(Goals,Variable,[X|R]) :- ask(Goals,Variable,R),!.
 
 xxx(F,W,[LL|L],BB) :- s([XX|X],F,[]),flatten([XX|X],XF), ask(W,[LL|L],XF), call(W), s([LL|X],BB,[]).
