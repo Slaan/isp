def2fa9d9ddc6bacb24442814af38deee612edac
Slaan <de.SlannesH@gmail.com>
2016-04-14 10:57:59 +0200

    asfd

--- aufg2/fabi/aufg2.pl
+++ aufg2/fabi/aufg2.pl
@@ -6,9 +6,9 @@ aS([SemNP,SemVP]) --> np(SemNP,N), vp(SemVP,N).
 ergF([SemVP,SemNP]) --> vp(SemVP,N), np(SemNP,N).
 entF([SemI,SemVP]) --> ip(SemI,_), vp(SemVP,_).
 
-np(SemNP,N) --> n(SemNP,N).
-np(SemNP,N) --> det(_), n(SemNP,N).
-np([SemN,SemPP],N) --> det(_), n(SemN,N), pp(SemPP,N).
+np(SemNP,N) --> n(SemNP,N,_).
+np(SemNP,N) --> det(_,_,_), n(SemNP,N,_).
+np([SemN,SemPP],N) --> det(_,Gen,Casus), n(SemN,N,Gen), pp(SemPP,N).
 
 pp(SemPP,N) --> p(_), np(SemPP,N).
 
@@ -16,29 +16,32 @@ vp(SemVP,N) --> v(SemVP,N).
 vp([SemN,SemNP],N) --> v(SemN,N), np(SemNP,_).
 
 pn(SemPN,N)--> [X], {lex(X,SemPN,pn,N)}.
-n(SemN,N)--> [X], {lex(X,SemN,na,N)}.
-n(SemN,N)--> [X], {lex(X,SemN,bz,N)}.
-v(SemV,N)--> [X], {lex(X,SemV,v,N)}.
-det(_)--> [X], {lex(X,_,det,_)}.
-ip(SemI,_)--> [X], {lex(X,SemI,ip,_)}.
-p(_)--> [X], {lex(X,_,p,_)}.
+n(SemN,N,Gen)--> [X], {lex(X,SemN,na,N,Gen,_)}.
+n(SemN,N,Gen)--> [X], {lex(X,SemN,bz,N,Gen,_)}.
+v(SemV,N)--> [X], {lex(X,SemV,v,N,_,_)}.
+det(_,Gen,Casus)--> [X], {lex(X,_,det,_,Gen,Casus)}.
+ip(SemI,_)--> [X], {lex(X,SemI,ip,_,_,_)}.
+p(_)--> [X], {lex(X,_,p,_,_,_)}.
 
-lex(wer,who,ip,_).
+lex(wer,who,ip,_,_,_).
 
-lex(ist,be,v,sg).
+lex(ist,be,v,sg,_,_).
 
-lex(der,the,det,_).
-lex(dem,the,det,_).
-lex(die,the,det,_).
+lex(der,the,det,_,male,nom).
+lex(die,the,det,_,female,nom).
+lex(der,the,det,_,female,dat).
+lex(dem,the,det,_,male,dat).
 
-lex(onkel,uncle,bz,sg).
-lex(tante,aunt,bz,sg).
-lex(vater,father,bz,sg).
+lex(onkel,uncle,bz,sg,male,_).
+lex(tante,aunt,bz,sg,female,_).
+lex(vater,father,bz,sg,male,_).
+lex(mutter,mother,bz,sg,female,_).
 
-lex(von,from,p,_).
+lex(von,from,p,_,_,_).
 
-lex(sam,sam,na,sg).
-lex(eve,eve,na,sg).
-lex(franz,franz,na,sg).
-lex(hans,hans,na,sg).
-lex(maria,maria,na,sg).
+lex(sam,sam,na,sg,male,_).
+lex(eve,eve,na,sg,female,_).
+lex(franz,franz,na,sg,male,_).
+lex(hans,hans,na,sg,male,_).
+lex(maria,maria,na,sg,female,_).
+lex(dan,dan,na,sg,male,_).
