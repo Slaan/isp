3cd2f263ed4df4be73155a6882f2c328f2965c8c
FBeck <FBeck>
2016-06-07 15:57:15 +0200

    aufg4
    Signed-off-by: FBeck <FBeck>

---
+++ aufg4/Datei1.pl
@@ -0,0 +1,33 @@
+% Autor:
+% Datum: 07.06.2016
+
+gleich(brite,rot).
+gleich(schwede,hund).
+gleich(dane,tee).
+
+links(gruen,weiss).
+
+gleich(gruen,kaffee).
+gleich(pallmall,vogel).
+gleich(3,milch).
+gleich(gelb,dunhill).
+gleich(norweger,1).
+
+neben(malboro,katze).
+neben(pferd,dunhill).
+
+gleich(winfield,bier).
+gleich(norweger,blau).
+gleich(deutsch,rothmanns).
+
+neben(malboro,wasser).
+
+
+
+tiere(vogel).
+
+
+
+
+
+
